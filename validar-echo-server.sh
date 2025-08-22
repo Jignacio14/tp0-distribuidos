70664073217c99913c350fc20df3f23130206fbb
Ignacio <j14castro2001@gmail.com>
2025-08-22 15:30:13 -0300

    feat: ajust bash script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-MESSAGE= "Hola probando 1 2 3, echooooooo"
-
-EXECUTION=$( docker run --rm --network="tp0_testing_net" busybox:latest sh -c "echo $MESSAGE | nc server 12345 " )
-
-SERVER_PORT = 12345
+MESSAGE="Hola probando 1 2 3, echooooooo"
+PORT=12345
+EXECUTION=$( docker run --rm --network="tp0_testing_net" busybox:latest sh -c "echo $MESSAGE | nc server $PORT " )
 
 if [ "$EXECUTION" == "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
