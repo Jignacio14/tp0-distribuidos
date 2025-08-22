09cbf5e8f0a63d803b9df979bdb650ee760ffc44
Ignacio <j14castro2001@gmail.com>
2025-08-22 12:49:07 -0300

    feat: finish ej1

--- compose-generator/generar-compose.py
+++ compose-generator/generar-compose.py
@@ -1,6 +1,6 @@
 import sys
 
-YAML_BODY = """"
+SERVER_YAML = """
 name: tp0
 services:
   server:
@@ -12,41 +12,60 @@ services:
       - LOGGING_LEVEL=DEBUG
     networks:
       - testing_net
+"""
 
-  client1:
-    container_name: client1
-    image: client:latest
-    entrypoint: /client
-    environment:
-      - CLI_ID=1
-      - CLI_LOG_LEVEL=DEBUG
-    networks:
-      - testing_net
-    depends_on:
-      - server
-
+NETWORK_YAML = """
 networks:
   testing_net:
     ipam:
       driver: default
       config:
         - subnet: 172.25.125.0/24
+
+"""
+
+CLIENT_YML = """
+client{id}:
+    container_name: client{id}
+    image: client:latest
+    entrypoint: python3 /main.py
+    environment:
+    - PYTHONUNBUFFERED=1
+    - LOGGING_LEVEL=DEBUG
+    - CLIENT_ID=client{id}
+    - SERVER_IP=server
+    networks:
+    - testing_net
 """
 
 def generate_script(file_destination, num_clients):
-    print("Client number:", num_clients)
-    print("File destination:", file_destination)
+
+    with open(file_destination, 'w') as f:
+        write_in_file(SERVER_YAML, f)
+        write_dinamically(num_clients, f)
+        write_in_file(NETWORK_YAML, f)
+
+def write_in_file(yml: str, file):
+    file.write(yml)
+
+def write_dinamically(num_clients: int, file):
+    for i in range(num_clients):
+        yaml = CLIENT_YML.format(id=i + 1)
+        write_in_file(yaml, file)
 
 if __name__ == "__main__":
     try:
         if len(sys.argv) != 3:
             print("You should call this script as: ./generar-compose.py <output_file> <num_clients>")
             sys.exit(1)
-        
         file_destination = sys.argv[1]
         client_nums = int(sys.argv[2])
         generate_script(file_destination, client_nums)
-
-    except Exception as e:
-        print("An error occurred:", e)
+        print(f"Compose file '{file_destination}' generated with {client_nums} clients.")
+        sys.exit(0)
+    except ValueError as err: 
+        print("You should provide a valid integer for the number of clients.")
         sys.exit(1)
+    except Exception as e:
+        print("An unexpected error occurred:", e)
+        sys.exit(2)
