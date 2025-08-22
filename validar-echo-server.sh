8d3f3c0dc7859aa2e13fe385d321678228775b52
Ignacio <j14castro2001@gmail.com>
2025-08-22 15:13:23 -0300

    feat: implement echo validar-echo-server

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,3 +1,13 @@
-echo "action: test_echo_server | result: success"
+#!/bin/bash
 
-echo "action: test_echo_server | result: fail"
+MESSAGE= "Hola probando 1 2 3, echooooooo"
+
+EXECUTION=$( docker run --rm --network="tp0_testing_net" busybox:latest sh -c "echo $MESSAGE | nc server 12345 " )
+
+SERVER_PORT = 12345
+
+if [ "$EXECUTION" == "$MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
