2c32754f76f71b2db7d5db44fbf4217ef235c066
Ignacio <j14castro2001@gmail.com>
2025-08-25 23:59:02 -0300

    feat: add basic protocol

---
+++ client/common/clientprotocol.go
@@ -0,0 +1,53 @@
+package common
+
+import "net"
+
+type Protocol struct {
+	conn net.Conn
+}
+
+func NewProtocol(serverAdr string) (*Protocol, error) {
+	conn, err := net.Dial("tcp", serverAdr)
+	if err != nil {
+		return nil, err
+	}
+
+	protocol := &Protocol{conn: conn}
+	return protocol, nil
+}
+
+func (p *Protocol) sendAll(data []byte) error {
+
+	len := len(data)
+
+	for sent := 0; sent < len; {
+		n, err := p.conn.Write(data[sent:])
+		if err != nil {
+			return err
+		}
+		sent += n
+	}
+
+	return nil
+}
+
+func (p *Protocol) receiveAll(len uint, array []byte) (int, error) {
+	received := 0
+
+	for received < int(len) {
+		n, err := p.conn.Read(array[received:])
+		if err != nil {
+			return 0, err
+		}
+		received += n
+	}
+
+	return received, nil
+}
+
+func (p *Protocol) shutdown() {
+	if p.conn == nil {
+		return
+	}
+	p.conn.Close()
+}
