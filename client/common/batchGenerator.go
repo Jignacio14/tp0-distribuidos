f652f4de542872c681ad71287fd14a7a3332e9d3
Ignacio <j14castro2001@gmail.com>
2025-08-28 00:38:53 -0300

    feat: add batch Generator

---
+++ client/common/batchGenerator.go
@@ -0,0 +1,66 @@
+package common
+
+import (
+	"bufio"
+	"os"
+)
+
+type BatchGenerator struct {
+	file      *os.File
+	scanner   *bufio.Scanner
+	currLine  int
+	isReading bool
+}
+
+func NewBatchGenerator(filePath string) (*BatchGenerator, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return &BatchGenerator{
+		file:      file,
+		scanner:   bufio.NewScanner(file),
+		currLine:  0,
+		isReading: false,
+	}, nil
+}
+
+func (bg *BatchGenerator) IsReading() bool {
+	return bg.isReading
+}
+
+func (bg *BatchGenerator) Read(batchSize int) (*Batch, error) {
+	batch := NewBatch(batchSize)
+	currLine := 0
+
+	for bg.scanner.Scan() {
+
+		if currLine < bg.currLine {
+			currLine++
+			continue
+		}
+
+		betStr := bg.scanner.Text()
+		bet, err := betFromString(betStr)
+
+		if err != nil {
+			return nil, err
+		}
+
+		err = batch.AddBet(*bet)
+
+		if err != nil {
+			return batch, nil
+		}
+
+		bg.currLine++
+	}
+
+	bg.isReading = false
+	return batch, nil
+}
+
+func (bg *BatchGenerator) Close() error {
+	return bg.file.Close()
+}
